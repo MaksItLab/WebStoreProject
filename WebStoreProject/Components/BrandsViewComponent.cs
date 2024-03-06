48cf71cfb83b99a2a78e151926917be11ff3d633
MaksItLab <maksim.ardintsev@mail.ru>
2024-03-06 11:00:30 +0300

    Модель представления секции

---
+++ WebStoreProject/Components/BrandsViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStoreProject.Components
+{
+	public class BrandsViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
---
+++ WebStoreProject/Components/SectionsViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStoreProject.Services.Interfaces;
+
+namespace WebStoreProject.Components
+{
+	
+	public class SectionsViewComponent : ViewComponent
+	{
+		public readonly IProductData _ProductData;
+        public SectionsViewComponent(IProductData ProductData) => _ProductData = ProductData;
+
+        public IViewComponentResult Invoke()
+		{
+			var sections = _ProductData.GetSections();
+
+			return View();
+		}
+	}
+}
