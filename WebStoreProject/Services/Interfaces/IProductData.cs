c8c3323bc10957fa5e60128bd1b3a81ed81208e8
MaksItLab <maksim.ardintsev@mail.ru>
2024-03-06 09:47:55 +0300

    Интерфейс сервиса каталога товаров

---
+++ WebStoreProject/Services/Interfaces/IProductData.cs
@@ -0,0 +1,10 @@
+﻿using WebStoreProject.Domain.Entities;
+
+namespace WebStoreProject.Services.Interfaces
+{
+	public interface IProductData
+	{
+		IEnumerable<Section> GetSections ();
+		IEnumerable<Brand> GetBrands ();
+	}
+}
