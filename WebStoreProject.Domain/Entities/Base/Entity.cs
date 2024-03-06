5c452c9988ad1214a896978440b8d03718366be8
MaksItLab <maksim.ardintsev@mail.ru>
2024-03-06 09:42:22 +0300

    базовые абстрактные классы

---
+++ WebStoreProject.Domain/Entities/Base/Entity.cs
@@ -0,0 +1,11 @@
+﻿
+using WebStoreProject.Domain.Entities.Base.Interfaces;
+
+namespace WebStoreProject.Domain.Entities.Base
+{
+	public abstract class Entity : IEntity
+	{
+		public int Id { get; set; }
+
+	}
+}
---
+++ WebStoreProject.Domain/Entities/Base/NamedEntity.cs
@@ -0,0 +1,11 @@
+﻿
+using WebStoreProject.Domain.Entities.Base.Interfaces;
+
+namespace WebStoreProject.Domain.Entities.Base
+{
+	public abstract class NamedEntity : Entity, INamedEntity
+	{
+		public string Name { get; set; }
+
+	}
+}
