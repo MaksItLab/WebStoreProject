d5d321c6a96ab215a307473bdc79c6cb1b620629
MaksItLab <maksim.ardintsev@mail.ru>
2024-03-06 11:09:33 +0300

    Логика визуального компонента формирования списка секций

--- WebStoreProject/Components/SectionsViewComponent.cs
+++ WebStoreProject/Components/SectionsViewComponent.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebStoreProject.Services.Interfaces;
+using WebStoreProject.ViewModels;
 
 namespace WebStoreProject.Components
 {
@@ -12,8 +13,35 @@ namespace WebStoreProject.Components
         public IViewComponentResult Invoke()
 		{
 			var sections = _ProductData.GetSections();
+			var parent_sections = sections.Where(s => s.ParentId is null);
+			var parent_sections_views = sections.
+				Select(s => new SectionViewModel
+				{
+					Id = s.Id,
+					Name = s.Name,
+					Order = s.Order,
+				})
+				.ToList();
 
-			return View();
+			foreach (var parent_section in parent_sections_views)
+			{
+				var childs = sections.Where(s => s.ParentId == parent_section.Id);
+				foreach (var child_section in childs)
+				{
+					parent_section.ChildSections.Add(new SectionViewModel
+					{
+						Id = child_section.Id,
+						Name = child_section.Name,
+						Order = child_section.Order,
+						Parent = parent_section,
+					});
+				}
+				parent_section.ChildSections.Sort((a,b) => Comparer<int>.Default.Compare(a.Order, b.Order));
+			}
+
+			parent_sections_views.Sort((a, b) => Comparer<int>.Default.Compare(a.Order, b.Order));
+
+			return View(parent_sections_views);
 		}
 	}
 }
